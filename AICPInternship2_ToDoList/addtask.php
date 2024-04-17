791bc689a9b2bb81a0f35830f4053a8027a767b4
Aamnakayani <145134126+Aamnakayani@users.noreply.github.com>
2024-04-18 00:09:15 +0500

    Create addtask.php

---
+++ AICPInternship2_ToDoList/addtask.php
@@ -0,0 +1,18 @@
+<?php
+include 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $sql = "INSERT INTO tasks (title, description) VALUES ('$title', '$description')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Task added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
